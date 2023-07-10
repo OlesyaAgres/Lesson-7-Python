a77011bcb74e33329c4510cc600185f0bfd20c6b
Olesya <olesyaspb.ru@mail.ru>
2023-07-10 09:44:59 +0300

    решение 2

--- task.py
+++ task.py
@@ -57,3 +57,23 @@
 
 # Решение
 
+# 1-вариант
+
+# def print_operation_table(operation, num_rows, num_сolumns):
+#     arr=[[operation(i,j) for i in range(1,num_rows+1)] for j in range(1, num_сolumns+1)]
+#     for i in arr:
+#         print(*[f"{x:>3}"for x in i])
+# line = int(input("Введите количество строк: "))
+# columns = int(input("Введите количество столбцов: "))
+# print_operation_table(lambda x,y: x*y,line,columns)
+
+# 2-вариант (с уточненным заданием (operation, num_rows=6, num_columns=6))
+
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+#     for i in a:
+#         print(*[f"{x:>3}" for x in i])
+
+
+# print_operation_table(lambda x, y: x * y)
+
